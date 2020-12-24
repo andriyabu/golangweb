e9584711044377ce95fa2cd4669f5a9d47510914
Andri Yabu <andriyabu@gmail.com>
2020-12-24 13:27:25 +0700

    template method for filtering stock

--- entity/product.go
+++ entity/product.go
@@ -7,3 +7,16 @@ type Product struct {
 	Price int
 	Stock int
 }
+
+//StockStatus is a method
+func (p Product) StockStatus() (status string) {
+	if p.Stock < 3 {
+		status = "Stock hampir habis"
+	} else if p.Stock < 10 {
+		status = "Stock terbatas"
+	} else {
+		status = "Ready banyak boss"
+	}
+
+	return
+}
